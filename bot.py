aa563423d2c2f74202743a5982333aefb8624324
MrKreg <taras.sheketa@gmail.com>
2019-01-23 22:51:20 +0200

    Switched to API

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 import telebot
 from telebot import types
 from telebot.types import Message
-import dbWorker
 from config import *
 from datetime import datetime
-
+import python_scheduler
 from config import COMMANDS
 
 bot = telebot.TeleBot(TOKEN)
@@ -19,9 +18,7 @@ def start_command(message: Message):
         name = message.from_user.username
     else:
         name = 'Незнайомець'
-    msg = bot.send_message(message.chat.id,
-                           'Вітаю ' + name +
-                           '''. Обери групу:''', reply_markup=choose_group_menu())
+    msg = bot.send_message(message.chat.id,'Вітаю ' + name + '.')
     bot.register_next_step_handler(msg, new_student)
 
 
@@ -39,9 +36,26 @@ def hide_buttons(message: Message):
     bot.send_message(message.chat.id, datetime.now().isoweekday(), reply_markup=types.ReplyKeyboardRemove())
 
 
+@bot.message_handler(commands=['week'])
+def week_command(message: Message):
+    bot.send_message(message.chat.id, str(python_scheduler.get_week(datetime.now(tz=UKRAINE))) + ' тиждень')
+
+
+@bot.message_handler(commands=['rings'])
+def switch_rings(message: Message):
+    if python_scheduler.RINGS == RINGSCHEDULE:
+        python_scheduler.RINGS = SHORTEDRINGSSCHEDULE
+    else:
+        python_scheduler.RINGS = RINGSCHEDULE
+    bot.send_message(message.chat.id, 'Успішно!')
+
+
 @bot.message_handler(commands=['test'])
 def test_command(message: Message):
-    schedule = dbWorker.get_schedule(message.from_user.id, 0)
+    if not python_scheduler.is_user_exist(message.chat.id):
+        bot.send_message(message.chat.id, 'Обери групу:', reply_markup=choose_group_menu())
+        bot.register_next_step_handler(message, new_student)
+    schedule = python_scheduler.get_tomorrow_schedule(get_user(message.chat.id))
     line = ''
     for sub in schedule:
         line += str(sub)
@@ -50,27 +64,46 @@ def test_command(message: Message):
 
 @bot.message_handler(commands=['change'])
 def change_command(message: Message):
+    if not python_scheduler.is_user_exist(message.chat.id):
+        bot.send_message(message.chat.id, 'Обери групу:', reply_markup=choose_group_menu())
+        bot.register_next_step_handler(message, new_student)
     msg = bot.send_message(message.chat.id, 'Обери групу:', reply_markup=choose_group_menu())
     bot.register_next_step_handler(msg, update_student)
 
 
 @bot.message_handler(commands=['check'])
 def check_command(message: Message):
-    group = dbWorker.get_user(message.from_user.id)
+    if not python_scheduler.is_user_exist(message.chat.id):
+        bot.send_message(message.chat.id, 'Обери групу:', reply_markup=choose_group_menu())
+        bot.register_next_step_handler(message, new_student)
+        return
+    group = python_scheduler.get_user(message.from_user.id)
     bot.send_message(message.chat.id,
                      'Ти обрав(ла) групу *' + group + '*.', parse_mode='Markdown')
 
 
 @bot.message_handler(content_types=['text'])
 def text_checker(message: Message):
+    if not python_scheduler.is_user_exist(message.chat.id):
+        bot.send_message(message.chat.id, 'Обери групу:', reply_markup=choose_group_menu())
+        bot.register_next_step_handler(message, new_student)
+        return
     global response
     text = str(message.text)
     if text == 'Сьогодні':
-        schedule = dbWorker.get_schedule(message.from_user.id, 0)
+        schedule = python_scheduler.get_today_schedule(python_scheduler.get_user(message.chat.id))
         response = prepare_schedule(schedule)
     elif text == 'Завтра':
-        schedule = dbWorker.get_schedule(message.from_user.id, 1)
+        schedule = python_scheduler.get_tomorrow_schedule(python_scheduler.get_user(message.chat.id))
         response = prepare_schedule(schedule)
+    elif text == 'Тиждень':
+        schedule = python_scheduler.get_current_week_schedule(python_scheduler.get_user(message.chat.id))
+        i = datetime.now(tz=UKRAINE).isoweekday()
+        for day in schedule:
+            response = WEEKDAYS[i] + '\n' + prepare_schedule(day)
+            i += 1
+            bot.send_message(message.chat.id, response, parse_mode='Markdown')
+        return
     elif text == 'Розклад дзвінків':
         response = prepare_ring_schedule()
     else:
@@ -79,11 +112,11 @@ def text_checker(message: Message):
 
 
 def new_student(message: Message):
-    if message.text not in dbWorker.get_all_groups():
+    if message.text not in python_scheduler.get_all_groups():
         msg = bot.send_message(message.chat.id, 'Хитрий, да? Обери будь ласка групу із списку внизу)')
         bot.register_next_step_handler(msg, new_student)
         return
-    if dbWorker.create_user(message.from_user.id, message.text):
+    if python_scheduler.create_user(message.from_user.id, message.text) == 201:
         bot.send_message(message.chat.id, 'Тепер ти можеш користуватися моїми можливостями)', reply_markup=main_menu())
     else:
         bot.send_message(message.chat.id, 'Ти можеш змінити групу за допомогою команди /change',
@@ -91,11 +124,15 @@ def new_student(message: Message):
 
 
 def update_student(message: Message):
-    if message.text not in dbWorker.get_all_groups():
+    if not python_scheduler.is_user_exist(message.chat.id):
+        bot.send_message(message.chat.id, 'Обери групу:', reply_markup=choose_group_menu())
+        bot.register_next_step_handler(message, new_student)
+        return
+    if message.text not in python_scheduler.get_all_groups():
         msg = bot.send_message(message.chat.id, 'Хитрий, да? Обери будь ласка групу із списку внизу)')
         bot.register_next_step_handler(msg, update_student)
         return
-    if dbWorker.update_user(message.from_user.id, message.text):
+    if python_scheduler.update_user(message.from_user.id, message.text) == 200:
         bot.send_message(message.chat.id, 'Групу успішно змінено)', reply_markup=main_menu())
     else:
         bot.send_message(message.chat.id, 'Здається щось пішло не так(', reply_markup=main_menu())
@@ -103,7 +140,7 @@ def update_student(message: Message):
 
 def choose_group_menu():
     markup = types.ReplyKeyboardMarkup()
-    for group in dbWorker.get_all_groups():
+    for group in python_scheduler.get_all_groups():
         markup.add(types.KeyboardButton(group))
     return markup
 
@@ -111,7 +148,7 @@ def choose_group_menu():
 def main_menu():
     markup = types.ReplyKeyboardMarkup()
     markup.row('Сьогодні', 'Завтра')
-    markup.row('Розклад дзвінків')
+    markup.row('Тиждень', 'Розклад дзвінків')
     return markup
 
 
@@ -125,7 +162,7 @@ def prepare_schedule(schedule):
 def prepare_ring_schedule():
     line = ''
     for i in range(1, 8):
-        line += '*' + str(i) + ' пара* ' + RINGSCHEDULE[i] + '\n'
+        line += '*' + str(i) + ' пара* ' + python_scheduler.RINGS[i] + '\n'
     return line
 
 
---
+++ python_scheduler.py
@@ -0,0 +1,98 @@
+import requests
+from datetime import datetime, timedelta
+from config import *
+from decimal import Decimal, ROUND_CEILING
+
+RINGS = RINGSCHEDULE
+
+class Lesson:
+    def __init__(self, lesson):
+        self.number = lesson['number']
+        self.subject = lesson['subject']
+        self.teacher = lesson['teacher']
+        self.classroom = lesson['classroom']
+
+    def __str__(self):
+        return '*' + str(self.number) + ' пара* ' + RINGS[self.number] + '\n_' + self.subject + '_  *' + self.teacher + '* _' + str(
+            self.classroom) + '_\n'
+
+
+def get_user(uid):
+    response = requests.get(URL + 'users/' + str(uid))
+    return response.json()['group']
+
+
+def create_user(uid, group_name):
+    params = {
+        'pk': uid,
+        'group': group_name
+    }
+    response = requests.post(URL + 'users/', data=params)
+    return response.status_code
+
+
+def update_user(uid, group_name):
+    params = {
+        'pk': uid,
+        'group': group_name
+    }
+    response = requests.put(URL + 'users/' + str(uid), data=params)
+    return response.status_code
+
+
+def is_user_exist(uid):
+    response = requests.get(URL + 'users/' + str(uid))
+    if response.status_code == 200:
+        return True
+    else:
+        return False
+
+
+def get_week(date):
+    return (Decimal((date - datetime(2018, 9, 2, tzinfo=UKRAINE)).days / 7).quantize(0, ROUND_CEILING) % 4)
+
+
+def get_one_day_schedule(group_name, weekday, week):
+    simplified_week = 5
+    schedule = []
+    if week > 4:
+        week = 1
+    elif week % 2 == 0:
+        simplified_week = 6
+    request = URL + 'lessons/?week=0&week={}&week={}&group_name={}&day={}'.format(week,simplified_week,group_name,weekday)
+    response = requests.get(request)
+    for lesson in response.json():
+        schedule.append(Lesson(lesson))
+    return schedule
+
+
+def get_today_schedule(group_name):
+    weekday = datetime.now(tz=UKRAINE).isoweekday()
+    week = get_week(datetime.now(tz=UKRAINE))
+    if weekday > 6:
+        return 'Пар немає)'
+    return get_one_day_schedule(group_name, weekday, week)
+    
+def get_tomorrow_schedule(group_name):
+    weekday = datetime.now(tz=UKRAINE).isoweekday()
+    week = get_week(datetime.now(tz=UKRAINE))
+    if weekday > 6:
+        return 'Пар немає)'
+    return get_one_day_schedule(group_name, weekday + 1, week)
+
+def get_current_week_schedule(group_name):
+    weekday = datetime.now(tz=UKRAINE).isoweekday()
+    week = get_week(datetime.now(tz=UKRAINE))
+    if weekday > 6:
+        return 'Пар немає)'
+    weekschedule = []
+    for i in range(weekday, 6):
+        weekschedule.append(get_one_day_schedule(group_name, i, week))
+    return weekschedule
+
+def get_all_groups():
+    response = requests.get(URL + 'groups/')
+    groups = []
+    for group in response.json():
+        groups.append(group['name'])
+    return groups
